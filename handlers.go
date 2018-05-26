0594ec3b824cc14d405717de92ae518889ce7769
GregoryDosh <GregoryDosh@users.noreply.github.com>
2018-05-25 23:41:55 -0500

    accounting for forwarded ip

--- handlers.go
+++ handlers.go
@@ -11,7 +11,11 @@ var filesHandler = fasthttp.FSHandler(staticFilePath, 0)
 
 func routeHandler(ctx *fasthttp.RequestCtx) {
 	// 127.0.0.1 user-identifier frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326
-	log.Infof("%s %s [%s] %s \"%s\"", ctx.RemoteIP(), ctx.UserAgent(), time.Now().Format("02/01/2006 15:04:05 -0700"), ctx.Method(), ctx.Path())
+	userIP := ctx.RemoteIP().String()
+	if tempIP := string(ctx.Request.Header.Peek("X-Forwarded-For")); tempIP != "" {
+		userIP = tempIP
+	}
+	log.Infof("%s %s [%s] %s \"%s\"", userIP, ctx.UserAgent(), time.Now().Format("02/01/2006 15:04:05 -0700"), ctx.Method(), ctx.Path())
 	switch string(ctx.Path()) {
 	case "/departures":
 		busHandler(ctx)
